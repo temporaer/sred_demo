78029ec596d2fb5ddfd2045c149ca7d7aa6cfd72
Hannes Schulz <hannes.schulz@maluuba.com>
2016-04-01 15:24:41 -0400

    minor

--- train_and_eval_model.py
+++ train_and_eval_model.py
@@ -5,7 +5,7 @@ import numpy as np
 exp_dir = os.environ['EXP_DIR']
 logging.basicConfig(filename=join(exp_dir, "log.txt"), level=logging.INFO)
 
-with open(join(exp_dir, "model.txt")) as f:
+with open(join(exp_dir, "model.txt"), "w") as f:
     f.write("this model has no params\n")
 
 logging.warn("Result: %2.3f", np.random.uniform())
