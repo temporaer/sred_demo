860f8edbfb3e3eb64284452c20cdaf0d5fc92659
Hannes Schulz <hannes.schulz@maluuba.com>
2016-04-01 15:28:46 -0400

    minor

--- train_and_eval_model.py
+++ train_and_eval_model.py
@@ -4,8 +4,10 @@ import logging
 import numpy as np
 exp_dir = os.environ['EXP_DIR']
 logging.basicConfig(filename=join(exp_dir, "log.txt"), level=logging.INFO)
+logger = logging.getLogger("sred_demo")
+logger.addHandler(logging.StreamHandler())
 
 with open(join(exp_dir, "model.txt"), "w") as f:
     f.write("this model has no params\n")
 
-logging.warn("Result: %2.3f", np.random.uniform())
+logger.warn("Result: %2.3f", np.random.uniform())
