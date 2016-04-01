ad05a6dc83ca88b93e8fe7808960cfa71caacc37
Hannes Schulz <hannes.schulz@maluuba.com>
2016-04-01 15:23:34 -0400

    add logging, exp_dir

--- train_and_eval_model.py
+++ train_and_eval_model.py
@@ -1,2 +1,11 @@
+import os
+from os.path import join
+import logging
 import numpy as np
-print "Result: ", np.random.uniform()
+exp_dir = os.environ['EXP_DIR']
+logging.basicConfig(filename=join(exp_dir, "log.txt"), level=logging.INFO)
+
+with open(join(exp_dir, "model.txt")) as f:
+    f.write("this model has no params\n")
+
+logging.warn("Result: %2.3f", np.random.uniform())
